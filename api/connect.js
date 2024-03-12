5966336d93eeee33b006756622e3a2723a823a26
Omar Khaled <154939176+OmarKhaled-47@users.noreply.github.com>
2024-03-12 23:33:26 +0200

    connect change

--- api/connect.js
+++ api/connect.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "onlinehost",
+  host: "mysql-28fe2c48-react-app-c733.a.aivencloud.com",
   user: "avnadmin",
   password: "AVNS_coXrQSfwfQUoEP-bzoA",
   database: "defaultdb",
