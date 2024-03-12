4e22a7f9811b6b8e66110773c735e8d3b6cff83f
Omar Khaled <154939176+OmarKhaled-47@users.noreply.github.com>
2024-03-12 23:26:30 +0200

    db change

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1010419/",
-  database: "social",
+  host: "onlinehost",
+  user: "avnadmin",
+  password: "AVNS_coXrQSfwfQUoEP-bzoA",
+  database: "defaultdb",
 });
