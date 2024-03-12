46414cc51ac4b26a2ea15d36370722c416cfc3de
Omar Khaled <154939176+OmarKhaled-47@users.noreply.github.com>
2024-03-13 00:01:00 +0200

    connect change

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,9 @@
-import mysql from "mysql2";
+import mysql from "mysql";
 
 export const db = mysql.createConnection({
   host: "mysql-28fe2c48-react-app-c733.a.aivencloud.com",
   user: "avnadmin",
+  port: 18388,
   password: "AVNS_coXrQSfwfQUoEP-bzoA",
   database: "defaultdb",
 });
